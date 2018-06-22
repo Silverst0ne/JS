2acf534b026102ee151ea080cc61fbfeb8736b24
Silverst0ne <ilyxa983@rambler.ru>
2018-06-22 12:48:19 +0300

    mod

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,8 +1,6 @@
-var a, b, c, d, e, f;
-a = parseInt(prompt("Сколько у вас с собой денег?"));
-b = parseInt(prompt("Сколько вы купили яблок?"));
-c = parseInt(prompt("Сколько вы купили батонов?"));
-d = parseInt(prompt("Сколько стоит одно яблоко?")) * b;
-e = parseInt(prompt("Сколько стоит один батон?")) * c;
-f = a > (e + d);
-f;
+const number = parseInt(prompt("Введите число"));
+if (number >= 0)
+  console.log(number);
+else {
+  console.log(-number);
+}
